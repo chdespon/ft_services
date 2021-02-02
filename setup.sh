ba30c77bfefb00992ba2c5be3737194a0bfd8cb2
chdespon <chdespon@student.42.fr>
2021-02-02 15:42:47 +0100

    pas grand chose

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# sudo groupadd docker
+
+# sudo usermod -aG docker $USER
 
 if [ $(minikube status | grep -c Running) != 3 ]
 then
@@ -11,13 +14,7 @@ kubectl apply -f srcs/scripts/metallb.yaml
 fi
 sh srcs/scripts/delete_services.sh
 
-
 # docker kill $(docker ps -q)
-
-# sudo groupadd docker
-
-# sudo usermod -aG docker $USER
-
 
 # --cpus=2 --memory=2200MB --extra-config=apiserver.service-node-port-range=1-35000
 
--- srcs/container_mysql/srcs/setup_container.sh
+++ srcs/container_mysql/srcs/setup_container.sh
@@ -8,3 +8,5 @@ mv my.cnf /etc/my.cnf
 mysql_install_db --user=root --datadir=/var/lib/mysql
 
 mysqld --init_file=/init.sql
+
+mysql -u root wordpress < wordpress.sql
