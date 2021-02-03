08e84f94e647558c8c3f351ec8b42c3aee3d4255
chdespon <chdespon@student.42.fr>
2021-02-03 15:51:44 +0100

    up

--- srcs/scripts/build_containers.sh
+++ srcs/scripts/build_containers.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-docker build ./srcs/container_wordpress/ -t wordpress
-docker build ./srcs/container_nginx/ -t nginx
-docker build ./srcs/container_pma/ -t phpmyadmin
-docker build ./srcs/container_mysql/ -t mysql
+docker build ./srcs/container_wordpress/ -t wordpress > ~/ft_services/logs/wordpress_logs.log
+docker build ./srcs/container_nginx/ -t nginx > ~/ft_services/logs/nginx_logs.log
+docker build ./srcs/container_pma/ -t phpmyadmin > ~/ft_services/logs/pma_logs.log
+docker build ./srcs/container_mysql/ -t mysql > ~/ft_services/logs/mysql_logs.log
