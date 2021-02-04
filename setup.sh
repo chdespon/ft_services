b9140fa831404b617e201eafcd667e287bcdda6d
chdespon <chdespon@student.42.fr>
2021-02-04 11:19:10 +0100

    setupt pimp

--- setup.sh
+++ setup.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+BLUE="\e[1;34m"
+GREEN="\e[1;32m"
+WHITE="\e[0m"
+
 if [ $(grep "docker" /etc/group | grep -c $USER ) = 0 ]
 then
 sudo usermod -aG docker $USER
@@ -10,20 +14,31 @@ fi
 
 if [ $(minikube status | grep -c Running) != 3 ]
 then
+echo ${BLUE}"==Starting Minikube=="${WHITE}
 minikube start --driver=docker
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
-kubectl apply -f srcs/scripts/metallb.yaml
+kubectl apply -f srcs/metallb/metallb.yaml
+echo ${GREEN}"==Done==\n\n"${WHITE}
 fi
+
+echo ${BLUE}"==Deleting Minikube instances=="${WHITE}
 sh srcs/scripts/delete_services.sh
+echo ${GREEN}"==Done==\n\n"${WHITE}
 
 # docker kill $(docker ps -q)
 
 eval $(minikube -p minikube docker-env)
 
+echo ${BLUE}"==Build docker container=="${WHITE}
+echo ${WHITE}"You can check logs in logs directory"${WHITE}
 sh srcs/scripts/build_containers.sh
+echo ${GREEN}"==Done==\n\n"${WHITE}
 
+echo ${BLUE}"==Creating Minikube instaces=="${WHITE}
 sh srcs/scripts/apply_yaml.sh
+echo ${GREEN}"==Done==\n\n"${WHITE}
 
+echo ${BLUE}"==Starting dashboard=="${WHITE}
 minikube dashboard
