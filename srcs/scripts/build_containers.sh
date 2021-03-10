8e7012ca50fd6fc65a7b02234063a0804f233596
chdespon <chdespon@student.42.fr>
2021-03-10 16:13:25 +0100

    fix build docker

--- srcs/scripts/build_containers.sh
+++ srcs/scripts/build_containers.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-docker build ./srcs/container_wordpress/ -t wordpress > ~/ft_services/logs/wordpress_logs.log
-docker build ./srcs/container_nginx/ -t nginx > ~/ft_services/logs/nginx_logs.log
-docker build ./srcs/container_pma/ -t phpmyadmin > ~/ft_services/logs/pma_logs.log
-docker build ./srcs/container_mysql/ -t mysql > ~/ft_services/logs/mysql_logs.log
-docker build ./srcs/container_FTPS/ -t ftps > ~/ft_services/logs/ftps_logs.log
-docker build ./srcs/container_grafana/ -t grafana > ~/ft_services/logs/grafana_logs.log
-docker build ./srcs/container_influxdb/ -t influxdb > ~/ft_services/logs/influxdb_logs.log
-docker build ./srcs/container_telegraf/ -t telegraf > ~/ft_services/logs/telegraf_logs.log
+docker build ./srcs/container_wordpress/ -t wordpress > ./logs/wordpress_logs.log
+docker build ./srcs/container_nginx/ -t nginx > ./logs/nginx_logs.log
+docker build ./srcs/container_pma/ -t phpmyadmin > ./logs/pma_logs.log
+docker build ./srcs/container_mysql/ -t mysql > ./logs/mysql_logs.log
+docker build ./srcs/container_FTPS/ -t ftps > ./logs/ftps_logs.log
+docker build ./srcs/container_grafana/ -t grafana > ./logs/grafana_logs.log
+docker build ./srcs/container_influxdb/ -t influxdb > ./logs/influxdb_logs.log
+docker build ./srcs/container_telegraf/ -t telegraf > ./logs/telegraf_logs.log
